149b47fcacb12cd4385b6c36e7c821c4f463d8ce
Armando <ooarmndooo@gmail.com>
2024-09-17 17:14:59 -0400

    creacion de la funcion multiplicar en una rama propia

--- aritmetica.py
+++
@@ -1,8 +0,0 @@
-# Realisado por Leyton cespedes
-def suma(a,b):
-    return a+b
-    
-# Realisado por Orlando Altamirano
-def multiplicacion(a,b):
-    return a*b
-
--- multiplicar.py
+++ multiplicar.py
@@ -4,3 +4,6 @@ def multiplicar():
     numero1 = int(input("ingrese el primer numero: "))
     numero2 = int(input("ingrese el segundo numero: "))
     return numero1 * numero2
+
+
+# print(f"el resultado de la operacion es: {multiplicar()}")
