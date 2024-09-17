cb36e22898b23481b97c5432be92388aa8f44fbd
Armando <ooarmndooo@gmail.com>
2024-09-17 12:22:20 -0400

    adicion de la funcion SUMA

---
+++ suma.py
@@ -0,0 +1,7 @@
+def sumar():
+    numero1 = int(input("ingrese el 1er numero a sumar: "))
+    numero2 = int(input("ingrese el 2do numero a sumar : "))
+    return numero1 + numero2
+
+# prueba
+# print(sumar())
