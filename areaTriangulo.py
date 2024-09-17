dcdedd7d54b779da1edbb2e62fd8a6147083a455
Saul Alejandro Peñaloza Lujan <saulalejandro0103@gmail.com>
2024-09-17 17:58:22 +0200

    Agregando area de un triangulo

---
+++ areaTriangulo.py
@@ -0,0 +1,6 @@
+def areaTriangulo():
+    
+    numero1 = float(input("ingrese la base: "))
+    numero2 = float(input("ingrese la altura: "))
+    return (numero1 * numero2) / 2
+print(areaTriangulo())
