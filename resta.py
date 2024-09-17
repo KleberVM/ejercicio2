b09e3a571c56054a4f836a6fbc485fcfbf1100a6
GUILDER <pplg39395@gmail.com>
2024-09-17 12:38:24 -0400

    Correccion de resta

--- resta.py
+++ resta.py
@@ -1,7 +1,7 @@
+# Desarrolado por Guilder Paredes Lovera
 def restar():
     numero1 = int(input("ingrese el 1er numero a sumar: "))
     numero2 = int(input("ingrese el 2do numero a sumar : "))
     return numero1 + numero2
 
-# Desarrollado por Guilder
-# print(sumar())
+# print(restar())
