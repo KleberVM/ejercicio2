7281864b1b60a64c6457c26587d74134a538d279
Saul Alejandro Peñaloza Lujan <saulalejandro0103@gmail.com>
2024-09-17 18:56:23 +0200

    area de rectangulo Saul

---
+++ areaRectangulo.py
@@ -0,0 +1,6 @@
+def areaRectangulo():
+    
+    numero1 = float(input("ingrese la base: "))
+    numero2 = float(input("ingrese la altura: "))
+    return (numero1 * numero2) 
+print(areaRectangulo())
