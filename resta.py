1540f798602742885e382fdb4a870cd3fd75feb6
GUILDER <pplg39395@gmail.com>
2024-09-17 12:33:46 -0400

    Resta implementado

---
+++ resta.py
@@ -0,0 +1,7 @@
+def restar():
+    numero1 = int(input("ingrese el 1er numero a sumar: "))
+    numero2 = int(input("ingrese el 2do numero a sumar : "))
+    return numero1 + numero2
+
+# Desarrollado por Guilder
+# print(sumar())
