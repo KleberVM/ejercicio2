526035d643e6c907fa9f689c16222dea792be8bf
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2024-09-17 13:14:56 -0400

    Adicion de la funcion MCD

---
+++ MCD.py
@@ -0,0 +1,11 @@
+def mcd():
+    # Solicitar al usuario que ingrese dos números
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+
+    # Calcular el MCD utilizando el algoritmo de Euclides
+    while num2:
+        num1, num2 = num2, num1 % num2
+    
+    # Retornar el resultado
+    return num1
