33a4825bb0c6af30c230f7c69ff4fc756c15556f
Yahir772 <leonardocayola@gmail.com>
2024-09-17 15:21:13 -0600

    Delete division.py

--- division.py
+++
@@ -1,8 +0,0 @@
-#Desarrollado por Yahir Leonardo Cayola Cayo
-def dividir():
-    num1 = int(input("ingrese el 1er numero a sumar: "))
-    num2 = int(input("ingrese el 2do numero a sumar : "))
-    if num2 != 0:
-        return num1 / num2
-    else:
-        return "Error: No se puede dividir entre cero."
