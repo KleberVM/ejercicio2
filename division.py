b29484f4a106f748523e3ae702b3f474553b5294
Yahir772 <leonardocayola@gmail.com>
2024-09-17 11:05:50 -0600

    Update division.py

--- division.py
+++ division.py
@@ -1,13 +1,9 @@
-def dividir():
+#Desarrollado por Yahir Leonardo Cayola Cayo 
+def division(numerador, denominador):
     try:
-        numerador = float(input("Ingresa el numerador: "))
-        denominador = float(input("Ingresa el denominador: "))
         resultado = numerador / denominador
-        print(f"El resultado de la división es: {resultado}")
+        return f"El resultado de la división es: {resultado}"
     except ZeroDivisionError:
-        print("Error: No se puede dividir entre cero.")
-    except ValueError:
-        print("Error: Debes ingresar valores numéricos.")
-
-# Ejecutar el programa
-dividir()
+        return "Error: No se puede dividir entre cero."
+    except TypeError:
+        return "Error: Los valores deben ser números."
